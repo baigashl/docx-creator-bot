bc4aa638cdd89f96bd413f48d1842c4b052664c6
Your Name <you@example.com>
2022-12-07 15:36:25 +0600

    asd

--- get_data.py
+++ get_data.py
@@ -60,33 +60,36 @@ async def get_delivery_data_from_api():
 
 
 async def get_pickup_data_from_api():
-    s = await make_request()
-    headers2 = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-        'Content-Type': 'application/json',
-    }
-    pickup_url = 'https://kaspi.kz/mc/api/orderTabs/active?count=50&selectedTabs=PICKUP&startIndex=0&returnedToWarehouse=false'
-    detail = 'https://kaspi.kz/merchantcabinet/api/order/details/%7Bstatus%7D/229325797'
+    try:
+        s = await make_request()
+        headers2 = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+            'Content-Type': 'application/json',
+        }
+        pickup_url = 'https://kaspi.kz/mc/api/orderTabs/active?count=50&selectedTabs=PICKUP&startIndex=0&returnedToWarehouse=false'
+        detail = 'https://kaspi.kz/merchantcabinet/api/order/details/%7Bstatus%7D/229325797'
 
-    request_data = s.get(
-        pickup_url,
-        headers=headers2,
-        cookies=s.cookies.get_dict()
-    )
-
-    with open('pickup.json', 'w', encoding='utf-8') as my_json:
-        json.dump(request_data.json(), my_json, ensure_ascii=False, indent=4)
-
-    with open('delivery.json') as json_file:
-        pickup_data = json.load(json_file)
-
-
-    for prod in pickup_data[0]["orders"]:
-        detail_data = s.get(
-            'https://kaspi.kz/merchantcabinet/api/order/details/%7Bstatus%7D/' + str(prod["orderCode"]),
+        request_data = s.get(
+            pickup_url,
             headers=headers2,
             cookies=s.cookies.get_dict()
         )
-        with open(f'{prod["orderCode"]}_pickup_prod_detail.json', 'w', encoding='utf-8') as my_json:
-            json.dump(detail_data.json(), my_json, ensure_ascii=False, indent=4)
+
+        with open('pickup.json', 'w', encoding='utf-8') as my_json:
+            json.dump(request_data.json(), my_json, ensure_ascii=False, indent=4)
+
+        with open('pickup.json') as json_file:
+            pickup_data = json.load(json_file)
+
+
+        for prod in pickup_data[0]["orders"]:
+            detail_data = s.get(
+                'https://kaspi.kz/merchantcabinet/api/order/details/%7Bstatus%7D/' + str(prod["orderCode"]),
+                headers=headers2,
+                cookies=s.cookies.get_dict()
+            )
+            with open(f'{prod["orderCode"]}_pickup_prod_detail.json', 'w', encoding='utf-8') as my_json:
+                json.dump(detail_data.json(), my_json, ensure_ascii=False, indent=4)
+    except KeyError:
+        return 'empty'
 
--- main.py
+++ main.py
@@ -11,9 +11,11 @@ from config import dp, bot
 from database.database import Order
 from get_data import get_delivery_data_from_api, get_pickup_data_from_api
 from docx.shared import Pt, Cm, Inches, Mm
+import docx2txt
 
-button4 = KeyboardButton('🚚 Доставка')
-markup3 = ReplyKeyboardMarkup(resize_keyboard=True).add(button4)
+button1 = KeyboardButton('🚚 Доставка')
+button2 = KeyboardButton('Самовывоз')
+markup3 = ReplyKeyboardMarkup(resize_keyboard=True).add(button1, button2)
 
 
 @dp.message_handler(commands=['start'])
@@ -26,7 +28,6 @@ async def add_db(message: types.Message):
     id_user = message.from_user.id
     await bot.send_message(id_user, "Немного подождите 😊")
     await get_delivery_data_from_api()
-    await get_delivery_data_from_api()
     with open('delivery.json') as json_file:
         delivery_data = json.load(json_file)
 
@@ -37,6 +38,12 @@ async def add_db(message: types.Message):
     section.page_width = Mm(105)
     # section.header_distance = Mm(12.7)
     # section.footer_distance = Mm(12.7)
+
+    font_styles = doc.styles
+    font_charstyle = font_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
+    font_object = font_charstyle.font
+    font_object.size = Pt(9)
+    font_object.name = 'Helvetica'
     for section in sections:
         section.top_margin = Inches(0.2)
         section.bottom_margin = Inches(0)
@@ -54,17 +61,8 @@ async def add_db(message: types.Message):
                 with open(f'{order["orderCode"]}_prod_detail.json') as json_file:
                     detail_data = json.load(json_file)
 
-                # print(order["orderCode"])
-
-
                 paragraph = doc.add_paragraph(f'Номер заказа\n')
                 paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-
-                font_styles = doc.styles
-                font_charstyle = font_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
-                font_object = font_charstyle.font
-                font_object.size = Pt(9)
-                font_object.name = 'Helvetica'
 
                 paragraph.add_run(f'{detail_data["orderId"]}\n', style='CommentsStyle')
                 paragraph.add_run(f'Сумма\n', style='CommentsStyle').bold = True
@@ -93,7 +91,7 @@ async def add_db(message: types.Message):
                 paragraph.add_run(f'Адрес доставки\n', style='CommentsStyle').bold = True
                 paragraph.add_run('\n', style='CommentsStyle')
                 paragraph.add_run(f'{detail_data["deliveryAddress"]["formattedAddress"]}\n', style='CommentsStyle')
-                paragraph.add_page_break()
+                doc.add_page_break()
                 # doc.center()
 
                 os.remove(f'{order["orderCode"]}_prod_detail.json')
@@ -103,12 +101,107 @@ async def add_db(message: types.Message):
 
         doc.save(f'delivery.docx')
 
-        await bot.send_document(id_user, document=open(f'delivery.docx', 'rb'))
+        my_text = docx2txt.process("delivery.docx")
+        if len(my_text) < 1:
+            await bot.send_message(id_user, "На данный момент нет НОВЫХ доставок 🙃")
+        else:
+            await bot.send_document(id_user, document=open(f'delivery.docx', 'rb'))
+
         os.remove(f'delivery.docx')
         # await bot.send_file()
         await bot.send_message(id_user, "Все данные были добавлены в Базу данных 😉 👌")
     except KeyError:
         await bot.send_message(id_user, "На данный момент нет доставок 🙃")
+
+
+
+@dp.message_handler(Text(equals='Самовывоз'))
+async def pickup(message: types.Message):
+    id_user = message.from_user.id
+    await bot.send_message(id_user, "Немного подождите 😊")
+    await get_pickup_data_from_api()
+    with open('pickup.json') as json_file:
+        delivery_data = json.load(json_file)
+
+    doc = docx.Document()
+    sections = doc.sections
+    section = sections[0]
+    section.page_height = Mm(148)
+    section.page_width = Mm(105)
+    # section.header_distance = Mm(12.7)
+    # section.footer_distance = Mm(12.7)
+
+    font_styles = doc.styles
+    font_charstyle = font_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
+    font_object = font_charstyle.font
+    font_object.size = Pt(9)
+    font_object.name = 'Helvetica'
+    for section in sections:
+        section.top_margin = Inches(0.2)
+        section.bottom_margin = Inches(0)
+        section.left_margin = Inches(0.2)
+        section.right_margin = Inches(0.2)
+    try:
+        # print(delivery_data[0]["orders"][0]["orderCode"])
+        for order in delivery_data[0]["orders"]:
+            code = Order.get_or_none(orderCode=order["orderCode"])
+            if code is None:
+
+                Order.create(
+                    orderCode=order["orderCode"],
+                )
+                with open(f'{order["orderCode"]}_pickup_prod_detail.json') as json_file:
+                    detail_data = json.load(json_file)
+
+                paragraph = doc.add_paragraph(f'Номер заказа\n')
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+                paragraph.add_run(f'{detail_data["orderId"]}\n', style='CommentsStyle')
+                paragraph.add_run(f'Сумма\n', style='CommentsStyle').bold = True
+                paragraph.add_run(f'{detail_data["localizedSum"]}\n', style='CommentsStyle')
+                paragraph.add_run('\n', style='CommentsStyle')
+                paragraph.add_run(f'Стоимость доставки для клиента\n', style='CommentsStyle')
+                paragraph.add_run(f'{detail_data["deliveryDiscountFormatted"]}\n', style='CommentsStyle')
+                paragraph.add_run('\n', style='CommentsStyle')
+                paragraph.add_run(f'Имя\n', style='CommentsStyle')
+                paragraph.add_run(f'{detail_data["purchaserFirstName"]} ', style='CommentsStyle')
+                paragraph.add_run(f'{detail_data["purchaserLastName"]}\n', style='CommentsStyle')
+                paragraph.add_run(f'Номер телефона\n', style='CommentsStyle')
+                paragraph.add_run(f'+7 ({detail_data["purchaserPhoneNumber"][0:3]})-{detail_data["purchaserPhoneNumber"][3:6]}-{detail_data["purchaserPhoneNumber"][6:8]}-{detail_data["purchaserPhoneNumber"][8:10]}\n', style='CommentsStyle')
+                paragraph.add_run('\n', style='CommentsStyle')
+                #################################################################3
+                for product in detail_data["products"]:
+                    paragraph.add_run(f'{product["name"]}\n', style='CommentsStyle')
+                    paragraph.add_run(f'{product["quantity"]}                 ', style='CommentsStyle')
+                    paragraph.add_run(f'{product["localizedActualPrice"]}\n', style='CommentsStyle')
+                ########################################################
+                # paragraph.add_run(f'{order["entries"][0]["name"]}\n', style='CommentsStyle')
+                # paragraph.add_run(f'{order["entries"][0]["quantity"]}                 ', style='CommentsStyle')
+                # paragraph.add_run(f'{order["entries"][0]["totalPrice"]}\n', style='CommentsStyle')
+                ###############################################################################3
+                doc.add_page_break()
+                # doc.center()
+
+                os.remove(f'{order["orderCode"]}_pickup_prod_detail.json')
+            else:
+                os.remove(f'{order["orderCode"]}_pickup_prod_detail.json')
+                print('exist')
+
+        doc.save(f'pickup.docx')
+
+        my_text = docx2txt.process("pickup.docx")
+        if len(my_text) < 1:
+            await bot.send_message(id_user, "На данный момент нет НОВЫХ Самовывозов 🙃")
+        else:
+            await bot.send_document(id_user, document=open(f'pickup.docx', 'rb'))
+
+        os.remove(f'pickup.docx')
+        # await bot.send_file()
+        await bot.send_message(id_user, "Все данные были добавлены в Базу данных 😉 👌")
+    except KeyError:
+        await bot.send_message(id_user, "На данный момент нет Самовывоза 🙃")
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
